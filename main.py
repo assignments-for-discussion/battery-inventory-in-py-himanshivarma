5ee7deb1196c977299b9ae1fa84111e435f127a5
HIMANSHI VARMA <77986431+himanshivarma@users.noreply.github.com>
2023-09-12 13:38:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def count_batteries_by_health(present_capacities):
         else:
             counts["failed"] += 1
 
-    return counts      
+    return counts
 def test_bucketing_by_health():
     print("Counting batteries by SoH...\n")
     present_capacities = [115, 118, 80, 95, 91, 72]
